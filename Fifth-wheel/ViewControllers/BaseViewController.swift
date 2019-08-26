0e796571f06d47920c5f734a4bf7220fa0325be4
Joshua Sharp <sharp.josh@gmail.com>
2019-08-25 23:12:35 -0400

    Started Xcode project

---
+++ Fifth-wheel/ViewControllers/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Fifth-wheel
+//
+//  Created by Joshua Sharp on 8/24/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Do base theming here
+    }
+
+
+}
+
---
+++ Fifth-wheel/ViewControllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Fifth-wheel
+//
+//  Created by Joshua Sharp on 8/25/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
