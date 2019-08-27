d09bb9cecb9ab1070fe8481c1e8d39e5205ea14c
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-26 20:29:13 -0400

    added filemanager persistence with save function

--- Fifth-wheel/Fifth-wheel/Models/UserController.swift
+++ Fifth-wheel/Fifth-wheel/Models/UserController.swift
@@ -13,14 +13,20 @@ class UserController {
     //Properties
     var loggedInUser: User?
     
+    private var userInfo: URL? {
+        let fileManager = FileManager.default
+        guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else    { return nil }
+        return documentsDirectory.appendingPathComponent("fillout.plist")
+    }
 }
 
 // MARK: -CRUD
 extension UserController {
     
-    func createUser(username: String, password: String, landowner: Bool) -> User {
+    func createUser(username: String, password: String, landowner: Bool) {
         let user = User(username: username, password: password, landowner: landowner)
-        return user
+        loggedInUser = user
+        saveUser()
     }
     
     func updateUser(password: String?, landowner: Bool?, bio: String?, image: String?) {
@@ -32,7 +38,20 @@ extension UserController {
         loggedInUser?.bio = bio
         loggedInUser?.landowner = landowner
         loggedInUser?.imageURL = image
+        saveUser()
     }
+}
+
+//Data functions
+extension UserController {
     
-    
+    func saveUser(){
+        guard let url = userInfo else {return print("Url not created in directory")}
+        do {
+            let user = try PropertyListEncoder().encode(self.loggedInUser)
+            try user.write(to: url)
+        } catch {
+            NSLog("Error user data: \(error) ")
+        }
+    }
 }
