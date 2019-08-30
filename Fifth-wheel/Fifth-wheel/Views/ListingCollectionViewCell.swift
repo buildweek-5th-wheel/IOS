d5587fe7f336af12964f4626edc828b560f8496f
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-29 23:32:51 -0400

    Image view added back to collection view

--- Fifth-wheel/Fifth-wheel/Views/ListingCollectionViewCell.swift
+++ Fifth-wheel/Fifth-wheel/Views/ListingCollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ListingCollectionViewCell: UICollectionViewCell {
     
-    //@IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
@@ -23,12 +23,12 @@ class ListingCollectionViewCell: UICollectionViewCell {
     
     
     func updateViews() {
-        //guard let imageURL = listing?.imageUrl else {return print("No Image url")}
-//        if imageURL != "" {
-//            imageView.downloaded(from: imageURL)
-//        } else {
-//            imageView.image = UIImage(named: "GenericCamping")
-//        }
+        guard let imageURL = listing?.imageUrl else {return print("No Image url")}
+        if imageURL != "" {
+            imageView.downloaded(from: imageURL)
+        } else {
+            imageView.image = UIImage(named: "GenericCamping")
+        }
         let name = listing?.listingName
         nameLabel.text = name
         let description = listing?.description
